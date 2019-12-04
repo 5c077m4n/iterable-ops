45f76c14c0dd0289a0afaa5dcfda621c581b033b
Roee Shapira <roy.shap1@gmail.com>
2019-12-04 11:48:09 +0200

    Added prettier config.

--- src/lazy-iterator/index.spec.ts
+++ src/lazy-iterator/index.spec.ts
@@ -1,22 +1,22 @@
 import { from } from './index';
-import { map, filter } from '../ops/index';
+import { map, filter, find } from '../ops/index';
 import { range } from '../helpers/index';
 
 describe('LazyIterator', () => {
-	it('Should map an array', () => {
-		const arr = [...range(0, 9)];
-		const arrOut = from(arr)
-			.pipe(map((x: number) => x * 2))
-			.get();
+    it('Should map an array', () => {
+        const arr: number[] = [...range(0, 9)];
+        const arrOut = from(arr)
+            .pipe(map((x: number) => x * 2))
+            .get();
 
-		expect(arrOut).toEqual(expect.arrayContaining(arr.map((x: number) => x * 2)));
-	});
-	it('Should filter an array', () => {
-		const arr = [...range(0, 9)];
-		const arrOut = from(arr)
-			.pipe(filter((x: number) => x % 2))
-			.get();
+        expect(arrOut).toEqual(expect.arrayContaining(arr.map((x: number) => x * 2)));
+    });
+    it('Should filter an array', () => {
+        const arr: number[] = [...range(0, 9)];
+        const arrOut = from(arr)
+            .pipe(filter((x: number) => x % 2))
+            .get();
 
-		expect(arrOut).toEqual(expect.arrayContaining(arr.filter((x: number) => x % 2)));
-	});
+        expect(arrOut).toEqual(expect.arrayContaining(arr.filter((x: number) => x % 2)));
+    });
 });
