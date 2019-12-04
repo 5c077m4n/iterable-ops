ca371756d48c252b8f9f64262b3edef83a00764f
Roee Shapira <roy.shap1@gmail.com>
2019-12-04 14:19:43 +0200

    Added tests and validation.

--- src/lazy-iterator/index.spec.ts
+++ src/lazy-iterator/index.spec.ts
@@ -1,8 +1,16 @@
 import { from } from './index';
-import { map, filter, find } from '../ops/index';
+import { map, filter, find, concat, unique } from '../ops/index';
 import { range } from '../helpers/index';
 
 describe('LazyIterator', () => {
+    it('Should throw an error for a non-iterable input', () => {
+        try {
+            // @ts-ignore
+            from(123);
+        } catch (e) {
+            expect(e).toBeInstanceOf(Error);
+        }
+    });
     it('Should map an array', () => {
         const arr: number[] = [...range(0, 9)];
         const arrOut = from(arr)
@@ -18,5 +26,40 @@ describe('LazyIterator', () => {
             .get();
 
         expect(arrOut).toEqual(expect.arrayContaining(arr.filter((x: number) => x % 2)));
+    });
+    it('Should find in an array', () => {
+        const arr: number[] = [...range(0, 9)];
+        const arrOut = from(arr)
+            .pipe(find((x: number) => x % 2))
+            .get();
+
+        expect(arrOut).toEqual(expect.arrayContaining([arr.find((x: number) => x % 2)]));
+    });
+    it('Should concat two arrays', () => {
+        const arr: number[] = [...range(0, 9)];
+        const arrOut = from(arr)
+            .pipe(concat(arr))
+            .get();
+
+        expect(arrOut).toEqual(expect.arrayContaining(arr.concat(arr)));
+    });
+    it('Should only return the unique values', () => {
+        const arr: number[] = [...range(0, 9), ...range(0, 9), ...range(0, 9), ...range(0, 9)];
+        const arrOut = from(arr)
+            .pipe(unique())
+            .get();
+
+        expect(arrOut).toEqual(expect.arrayContaining([...new Set(arr).values()]));
+    });
+    it('Should use multiple pipe operators', () => {
+        const arr: number[] = [...range(0, 9)];
+        const arrOut = from(arr)
+            .pipe(
+                map((x: number) => x * 3),
+                filter((x: number) => x % 2),
+            )
+            .get();
+
+        expect(arrOut).toEqual(expect.arrayContaining(arr.map((x: number) => x * 3).filter((x: number) => x % 2)));
     });
 });
--- src/lazy-iterator/index.ts
+++ src/lazy-iterator/index.ts
@@ -1,32 +1,35 @@
 export class LazyIterable {
-	private _iter: Iterable<any>;
-	private _callbackList: PipeFunction<any, any>[] = [];
+    private _iter: Iterable<any>;
+    private _callbackList: PipeFunction<any, any>[] = [];
 
-	constructor(iter: Iterable<any>, options: object = {}) {
-		this._iter = iter;
-	}
+    constructor(iter: Iterable<any>, options: object = {}) {
+        if (iter && typeof iter[Symbol.iterator] !== 'function') {
+            throw new Error('The given input is not an iterable.');
+        }
+        this._iter = iter;
+    }
 
-	private *_calc(): Iterable<any> {
-		let result = this._iter;
-		for (const operation of this._callbackList) {
-			result = yield* operation(result);
-		}
-		return result;
-	}
+    private *_calc(): Iterable<any> {
+        let result = this._iter;
+        for (const operation of this._callbackList) {
+            result = yield* operation(result);
+        }
+        return result;
+    }
 
-	public pipe(...ops: PipeFunction[]): LazyIterable {
-		this._callbackList = ops;
-		return this;
-	}
+    public pipe(...ops: PipeFunction[]): LazyIterable {
+        this._callbackList = ops;
+        return this;
+    }
 
-	public get(cb?: Function): any[] {
-		const result = Array.from(this._calc());
+    public get(cb?: Function): any[] {
+        const result = Array.from(this._calc());
 
-		if (typeof cb === 'function') return cb(result);
-		return Array.from(result);
-	}
+        if (typeof cb === 'function') return cb(result);
+        return Array.from(result);
+    }
 }
 
 export function from(iter: Iterable<any>, options?: object): LazyIterable {
-	return new LazyIterable(iter, options);
+    return new LazyIterable(iter, options);
 }
